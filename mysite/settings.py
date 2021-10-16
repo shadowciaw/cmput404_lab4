5ebe849c635f794ef13059ee34ebcb3043e66bc2
Cynthia Li <home@Cynthias-MacBook-Air.local>
2021-10-15 21:34:09 -0600

    Added Heroku integration

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,3 +1,4 @@
+import django_on_heroku # top of the file
 """
 Django settings for mysite project.
 
@@ -124,3 +125,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_on_heroku.settings(locals()) # bottom of the file
